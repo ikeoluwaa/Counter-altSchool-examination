600661f0caf0b147421d94230a77a813d25b7d22
ikeoluwaa <ike_official@yahoo.com>
2023-10-22 14:03:06 +0100

    secound commit

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -6,11 +6,10 @@ import { LiaMinusSolid } from "react-icons/lia";
 function CounterApp() {
   const { count, increment, decrement, reset, setValue } = useCounter();
   const [inputValue, setInputValue] = useState("");
-  const maxValue = 1000000000000;
 
   const handleSetValue = () => {
     const parsedValue = Number(inputValue);
-    if (!isNaN(parsedValue) && parsedValue <= maxValue) {
+    if (!isNaN(parsedValue)) {
       setValue(parsedValue);
       setInputValue("");
     }
@@ -20,15 +19,10 @@ function CounterApp() {
     reset();
     setInputValue("");
   };
+
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       handleSetValue();
-    }
-  };
-  const handleInputChange = (event) => {
-    const value = event.target.value;
-    if (value <= maxValue || value === "") {
-      setInputValue(value);
     }
   };
 
@@ -36,21 +30,21 @@ function CounterApp() {
     <div className="container">
       <div className="counter-container">
         <h1>{count}</h1>
-        <div className=" input-container">
-          <button className=" buttons reset" onClick={handleReset}>
+        <div className="input-container">
+          <button className="buttons reset" onClick={handleReset}>
             Reset
           </button>
-          <button className=" buttons increment" onClick={increment}>
+          <button className="buttons increment" onClick={increment}>
             <IoAdd />
           </button>
-          <button className=" buttons decrement" onClick={decrement}>
+          <button className="buttons decrement" onClick={decrement}>
             <LiaMinusSolid />
           </button>
         </div>
         <input
           type="number"
           value={inputValue}
-          onChange={handleInputChange}
+          onChange={(e) => setInputValue(e.target.value)}
           onKeyPress={handleKeyPress}
           style={{ wordWrap: "break-word", overflowWrap: "break-word" }}
           placeholder="Enter a number"
@@ -62,4 +56,5 @@ function CounterApp() {
     </div>
   );
 }
+
 export default CounterApp;
